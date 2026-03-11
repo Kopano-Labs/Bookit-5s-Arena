13083fb0e48027040fae9796751e6a0cc5db7b0e
RobynAwesome <rkholofelo@gmail.com>
2026-03-11 13:40:07 +0200

    Double duplicate

--- bookit-app-main/components/AuthWrapper.jsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import { AuthProvider } from '@/context/authContext';
-
-const AuthWrapper = ({ children }) => {
-  return <AuthProvider>{children}</AuthProvider>;
-};
-
-export default AuthWrapper;
