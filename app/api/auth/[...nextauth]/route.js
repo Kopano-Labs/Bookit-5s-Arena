062ee212bfecb81e3b98f3782ca5201d416ee40c
RobynAwesome <rkholofelo@gmail.com>
2026-03-12 04:17:27 +0200

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,3 +4,7 @@ import { authOptions } from '@/lib/authOptions';
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
+
+import dns from 'node:dns/promises';
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
