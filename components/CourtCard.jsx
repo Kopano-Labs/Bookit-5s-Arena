3c5acfcfb81b0be06a4efe609d7af7a124b37af8
RobynAwesome <rkholofelo@gmail.com>
2026-03-12 16:50:46 +0200

    Updated CourtCard.jsx
    BookingForm now receives the real MongoDB _id so bookings will work

--- components/CourtCard.jsx
+++ components/CourtCard.jsx
@@ -27,7 +27,7 @@ const CourtCard = ({ court }) => {
       </div>
       <div className="flex flex-col sm:flex-row w-full sm:w-auto sm:space-x-2 mt-2 sm:mt-0">
         <Link
-          href={`/courts/${court.$id}`}
+          href={`/courts/${court._id}`}
           className="bg-blue-500 text-white px-4 py-2 rounded mb-2 sm:mb-0 w-full sm:w-auto text-center hover:bg-blue-700"
         >
           View Court
