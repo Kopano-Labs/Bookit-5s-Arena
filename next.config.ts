c2f62388dc8e92daa753d444c877f892129046b4
RobynAwesome <rkholofelo@gmail.com>
2026-03-14 02:03:30 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   turbopack: {},
+  allowedDevOrigins: ['192.168.101.106', '*.local', 'localhost'],
 };
 
 export default nextConfig;
