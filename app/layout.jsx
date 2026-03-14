c9d0b96afb96098565f049bfe4ce89cd24d87fb6
RobynAwesome <rkholofelo@gmail.com>
2026-03-14 02:35:19 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,7 +21,7 @@ const RootLayout = ({ children }) => {
       <body>
         <AuthProvider>
           <Header />
-          <main>
+          <main className="px-4 sm:px-6 lg:px-8 py-6">
             {children}
           </main>
           <Footer />
