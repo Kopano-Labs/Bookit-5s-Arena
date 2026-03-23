b78a56e189287d652bb0848d25ed715752dbaeea
RobynAwesome <rkholofelo@gmail.com>
2026-03-23 20:23:24 +0200

    Update ClientOnly.jsx

--- components/ClientOnly.jsx
+++ components/ClientOnly.jsx
@@ -1,13 +1,11 @@
 'use client';
 import dynamic from 'next/dynamic';
 
-const ChatWidget = dynamic(() => import('./ChatWidget'), { ssr: false });
 const Analytics = dynamic(() => import('./Analytics'), { ssr: false });
 
 export default function ClientOnly() {
   return (
     <>
-      <ChatWidget />
       <Analytics />
     </>
   );
